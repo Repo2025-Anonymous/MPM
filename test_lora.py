c4321db4794ff8da29450fd549910146f548a603
Vision <wuranwan2025@126.com>
2025-08-01 11:00:50 +0800

    Update test_lora.py

--- test_lora.py
+++ test_lora.py
@@ -6,7 +6,7 @@ from torch.nn import DataParallel
 from tqdm import tqdm
 import glob
 from data.dataset import FSSDataset
-from model.RanwanNet2 import LoRA_Sam
+from model.MPM import LoRA_Sam
 from segment_anything_lora import sam_model_registry
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "4"
@@ -14,7 +14,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "4"
 def parse_args():
     parser = argparse.ArgumentParser(description='IFA for CD-FSS')
     # basic arguments
-    parser.add_argument('--data-root', type=str, default='/home/ranwanwu/CDFSS/dataset',  help='root path of training dataset')
+    parser.add_argument('--data-root', type=str, default='../CDFSS/dataset',  help='root path of training dataset')
     parser.add_argument('--dataset', type=str, default='isic', choices=['fss', 'deepglobe', 'isic', 'lung'], help='training dataset')
     parser.add_argument('--backbone', type=str, default='resnet50', choices=['resnet50', 'resnet101'], help='backbone of semantic segmentation model')
     parser.add_argument('--shot', type=int, default=1, help='number of support pairs')
@@ -98,7 +98,7 @@ def main():
     FSSDataset.initialize(img_size=400, datapath=args.data_root)
     testloader = FSSDataset.build_dataloader(args.dataset, args.batch_size, 4, '0', 'val', args.shot)
 
-    sam = sam_model_registry["vit_b"](checkpoint='/home/ranwanwu/CDFSS/pretrained/sam_vit_b_01ec64.pth')  # "sam_vit_b_01ec64.pth")
+    sam = sam_model_registry["vit_b"](checkpoint='../CDFSS/pretrained/sam_vit_b_01ec64.pth')  # "sam_vit_b_01ec64.pth")
     sam = sam[0]
     model = LoRA_Sam(sam, 4, args.backbone, args.shot).cuda()
 
@@ -106,27 +106,27 @@ def main():
     if args.dataset == 'deepglobe':
         if args.backbone == 'resnet50':
             if args.shot == 1:
-                checkpoint_path = './outdir/RanwanNet/deepglobe/fine_tuning/resnet50_1shot_avg_50.50.pth'
+                checkpoint_path = './deepglobe/fine_tuning/resnet50_1shot_avg_50.50.pth'
             if args.shot == 5:
-                checkpoint_path = './outdir/RanwanNet/deepglobe/fine_tuning/resnet50_5shot_avg_58.61.pth'
+                checkpoint_path = './deepglobe/fine_tuning/resnet50_5shot_avg_58.61.pth'
     if args.dataset == 'isic':
         if args.backbone == 'resnet50':
             if args.shot == 1:
-                checkpoint_path = './outdir/RanwanNet/isic/fine_tuning/resnet50_1shot_avg_74.38.pth'
+                checkpoint_path = './isic/fine_tuning/resnet50_1shot_avg_74.38.pth'
             if args.shot == 5:
-                checkpoint_path = './outdir/RanwanNet/isic/fine_tuning/resnet50_5shot_avg_77.93.pth'
+                checkpoint_path = './isic/fine_tuning/resnet50_5shot_avg_77.93.pth'
     if args.dataset == 'lung':
         if args.backbone == 'resnet50':
             if args.shot == 1:
-                checkpoint_path = './outdir/RanwanNet/lung/fine_tuning/resnet50_1shot_avg_76.91.pth'
+                checkpoint_path = './lung/fine_tuning/resnet50_1shot_avg_76.91.pth'
             if args.shot == 5:
-                checkpoint_path = './outdir/RanwanNet/lung/fine_tuning/resnet50_5shot_avg_78.19.pth'
+                checkpoint_path = './lung/fine_tuning/resnet50_5shot_avg_78.19.pth'
     if args.dataset == 'fss':
         if args.backbone == 'resnet50':
             if args.shot == 1:
-                checkpoint_path = './outdir/RanwanNet/fss/fine_tuning/resnet50_1shot_avg_83.22.pth'
+                checkpoint_path = './fss/fine_tuning/resnet50_1shot_avg_83.22.pth'
             if args.shot == 5:
-                checkpoint_path = './outdir/RanwanNet/fss/fine_tuning/resnet50_5shot_avg_84.89.pth'
+                checkpoint_path = './fss/fine_tuning/resnet50_5shot_avg_84.89.pth'
 
     print('Evaluating model:', checkpoint_path)
 
